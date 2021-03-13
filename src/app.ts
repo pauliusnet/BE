8c51ead7fab7b21e6ef185c66887a231f49a1085
pauliusnet <47847408+pauliusnet@users.noreply.github.com>
2021-03-13 11:00:18 +0200

    fix: some namings and move app initiation to server.ts file (#1)

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,7 @@ import 'dotenv/config';
 
 const app = express();
 
-//Define routes
 app.use('/leaderboard', leaderboardRoutes);
 app.use('/userDetails', userRoutes);
-
-//Server start
-if (process.env.NODE_ENV != 'test') {
-    const port = 3000;
-    app.listen(port, () => {
-        console.log(`Server is running on localhost: ${port}`);
-    });
-}
 
 export default app;
